c03bb3f6b5d274b6ba4785d5406ebd39dfd265f0
Marc Duran Olivé <liamato97@gmail.com>
2015-10-19 03:32:31 +0200

    Added routes(without functionality), npm modules(react, react-dom, react-router, history, browserify, babelify) and gived 777 permisions to all project folder

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,196 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [
+	'uses' => '',
+	'as' => 'home',
+]);
+
+
+/*  --PerSchool routes--  */
+Route::group(['as' => 'school_', 'prefix' => '{school}'], function(){
+
+	Route::get('/', [
+		'uses' => '',
+		'as' => 'home',
+	]);
+
+	Route::get('/news', [
+		'uses' => '',
+		'as' => 'news',
+	]);
+
+	Route::get('/list', [
+		'uses' => '',
+		'as' => 'list',
+	]);
+
+	Route::get('/liberate', [
+		'uses' => '',
+		'as' => 'liberate',
+	]);
+
+	Route::get('/register', [
+		'uses' => '',
+		'as' => 'register',
+	]);
+
+	Route::get('/forum', [
+		'uses' => '',
+		'as' => 'forum',
+	]);
+
+	Route::get('/trailer', [
+		'uses' => '',
+		'as' => 'trailer',
+	]);
+
+	Route::get('/tube', [
+		'uses' => '',
+		'as' => 'tube',
+	]);
+
+	/*  --Admin routes--  */
+	Route::group(['as' => '', 'middleware' => 'admin', 'prefix' => 'admin'], function(){
+		Route::get('/', [
+			'uses' => '',
+			'as' => 'home',
+		]);
+
+		Route::get('/add', [
+			'uses' => '',
+			'as' => 'add',
+		]);
+
+		Route::get('/edit/{id?}', [
+			'uses' => '',
+			'as' => 'edit',
+		]);
+
+		Route::get('/remove', [
+			'uses' => '',
+			'as' => 'remove/{id?}',
+		]);
+
+		/*  --Admin profile routes--  */
+		Route::group(['as' => 'profile_', 'prefix' => 'profile'], function(){
+			Route::get('/', [
+				'uses' => '',
+				'as' => 'home',
+			]);
+		});
+
+		/*  --Admin news routes--  */
+		Route::group(['as' => 'news_', 'prefix' => 'news'], function(){
+			Route::get('/', [
+				'uses' => '',
+				'as' => 'home',
+			]);
+
+			Route::get('/add', [
+				'uses' => '',
+				'as' => 'add',
+			]);
+
+			Route::get('/edit/{id?}', [
+				'uses' => '',
+				'as' => 'edit',
+			]);
+
+			Route::get('/remove/{id?}', [
+				'uses' => '',
+				'as' => 'remove',
+			]);
+		});
+
+		/*  --Admin forum routes--  */
+		Route::group(['as' => 'forum_', 'prefix' => 'forum'], function(){
+			Route::get('/', [
+				'uses' => '',
+				'as' => 'home',
+			]);
+
+			Route::get('/add', [
+				'uses' => '',
+				'as' => 'add',
+			]);
+
+			Route::get('/edit/{id?}', [
+				'uses' => '',
+				'as' => 'edit',
+			]);
+
+			Route::get('/remove/{id?}', [
+				'uses' => '',
+				'as' => 'remove',
+			]);
+		});
+
+		Route::get('/trailer', [
+			'uses' => '',
+			'as' => 'trailer',
+		]);
+
+		Route::get('/tube', [
+			'uses' => '',
+			'as' => 'tube',
+		]);
+	});
+
+});
+
+
+
+/*  --SuperAdmin routes--  */
+Route::group(['as' => 'admin_', 'middleware' => 'superadmin', 'prefix' => 'admin'], function(){
+	Route::get('/', [
+		'uses' => '',
+		'as' => 'home',
+	]);
+
+	Route::get('/add', [
+		'uses' => '',
+		'as' => 'add',
+	]);
+
+	Route::get('/edit/{id?}', [
+		'uses' => '',
+		'as' => 'edit',
+	]);
+
+	Route::get('/remove/{id?}', [
+		'uses' => '',
+		'as' => 'remove',
+	]);
+
+	/*  --SuperAdmin profile routes--  */
+	Route::group(['as' => 'profile_', 'prefix' => 'profile'], function(){
+		Route::get('/', [
+			'uses' => '',
+			'as' => 'home',
+		]);
+	});
+
+	/*  --SuperAdmin school routes--  */
+	Route::group(['as' => 'school_', 'prefix' => 'school'], function(){
+		Route::get('/', [
+			'uses' => '',
+			'as' => 'home',
+		]);
+
+		Route::get('/add', [
+			'uses' => '',
+			'as' => 'add',
+		]);
+
+		Route::get('/edit/{id?}', [
+			'uses' => '',
+			'as' => 'edit',
+		]);
+
+		Route::get('/remove/{id?}', [
+			'uses' => '',
+			'as' => 'remove',
+		]);
+	});
 });
