b2b0357333dee02765391a0e4d62e385f31e674c
Marc Duran Olivé <liamato97@gmail.com>
2016-02-11 00:39:25 +0100

    Added forgeted Book Model

---
+++ app/Book.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    protected $fillable = ['title', 'author'];
+
+    protected $hidden = ['created_at', 'updated_at', 'checked'];
+
+    public function school()
+    {
+        return $this->belongsTo(School::class);
+    }
+
+    public function scopebySchool($q, $school)
+    {
+    	return $q->where('school_id', School::where((is_numeric($school) ? 'id' : 'slug'), $school)->firstOrFail()->id);
+    }
+}
